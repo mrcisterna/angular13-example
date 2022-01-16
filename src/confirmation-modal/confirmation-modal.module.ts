dbf682e4d13a7dd176b6cc9c249da166dba5b0b6
mrcisterna <martincisterna@hotmail.com>
2022-01-17 00:39:01 +0100

    Final improvements.

--- src/confirmation-modal/confirmation-modal.module.ts
+++ src/confirmation-modal/confirmation-modal.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SimpleConfirmationModalComponent } from './simple-confirmation-modal/simple-confirmation-modal.component';
 
-
-
 @NgModule({
   declarations: [
     SimpleConfirmationModalComponent
